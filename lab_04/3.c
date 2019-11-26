3761aed7113a2f8d30a70448a92eb1221cf08b3c
Willsem <happymainer132@yandex.ru>
2019-11-26 10:51:18 +0300

    [ADD] task 3 for lab_04

---
+++ lab_04/3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+int main()
+{
+    int status;
+    pid_t childpid1, childpid2;
+
+    childpid1 = fork();
+
+    if (childpid1 == -1)
+    {
+        perror("Can’t fork.\n");
+        return 1;
+    }
+    else if (childpid1 == 0)
+    {
+        execl("/bin/ls", "ls", "-lah", 0);
+        exit(0);
+    }
+
+    childpid2 = fork();
+
+    if (childpid2 == -1)
+    {
+        perror("Can't fork.\n");
+        return 1;
+    }
+    else if (childpid2 == 0)
+    {
+        execl("/bin/cat", "cat", "makefile", 0);
+        exit(0);
+    }
+    else
+    {
+        wait(&status);
+
+        if (WIFEXITED(status)) printf(ANSI_COLOR_GREEN "child process exit success\n" ANSI_COLOR_RESET);
+    }
+
+    return 0;
+}
