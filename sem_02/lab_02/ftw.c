4a74f0c84390e4eb55a353c3a24aadcc672159da
Willsem <happymainer132@yandex.ru>
2020-03-10 00:32:26 +0300

    :pencil: make iteration method

--- sem_02/lab_02/ftw.c
+++ sem_02/lab_02/ftw.c
@@ -8,22 +8,102 @@ int ftw(char *pathname, Function *func)
         return -1;
     }
 
-    return dopath(func, ".", 0);
+    return dopath(func);
 }
 
+int dopath(Function *func)
+{
+    struct stat     statbuf;
+    struct dirent   *dirp;
+    DIR             *dp;
+    int             ret;
+
+    int             len = 0;
+
+    struct stack *stack = stack_create();
+    stack_push(stack, ".");
+
+    while (!stack_is_empty(stack))
+    {
+        const char *filename = stack_pop(stack);
+
+        if (filename == "..")
+        {
+            len--;
+
+            if (chdir("..") < 0)
+            {
+                printf("Cannot return into .. from");
+                return -1;
+            }
+
+            continue;
+/*
+            if (closedir(dp) < 0)
+            {
+                printf("Can't close directory %s\n", filename);
+                return -1;
+            }
+*/
+        }
+
+        if (lstat(filename, &statbuf) < 0)
+        {
+            func(filename, &statbuf, FTW_NS, 0);
+            continue;
+        }
+
+        if (S_ISDIR(statbuf.st_mode) == 0)
+        {
+            func(filename, &statbuf, FTW_F, 0);
+            continue;
+        }
+
+        ret = func(filename, &statbuf, FTW_D, len);
+
+        dp = opendir(filename);
+
+        if (dp == NULL)
+        {
+            printf("DEBUG2");
+            func(filename, &statbuf, FTW_DNR, 0);
+        }
+
+        if (chdir(filename) < 0)
+        {
+            printf("Cannot chdir into %s\n", filename);
+            func(filename, &statbuf, FTW_DNR, 0);
+            continue;
+        }
+
+        ++len;
+
+        stack_push(stack, "..");
+        while ((dirp = readdir(dp)) != NULL)
+        {
+            if (strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0)
+            {
+                stack_push(stack, dirp->d_name);
+            }
+        }
+    }
+
+    return ret;
+}
+
+/*
 int dopath(Function *func, char *filename, int len)
 {
     struct stat     statbuf;
     struct dirent   *dirp;
     DIR             *dp;
     int             ret;
-    char            *ptr;
 
     if (lstat(filename, &statbuf) < 0)
-        return(func(filename, &statbuf, FTW_NS, 0));
+        return func(filename, &statbuf, FTW_NS, 0);
 
     if (S_ISDIR(statbuf.st_mode) == 0)
-        return(func(filename, &statbuf, FTW_F, 0));
+        return func(filename, &statbuf, FTW_F, 0);
 
     ret = func(filename, &statbuf, FTW_D, len);
 
@@ -65,3 +145,4 @@ int dopath(Function *func, char *filename, int len)
 
     return ret;
 }
+*/
--- sem_02/lab_02/ftw.h
+++ sem_02/lab_02/ftw.h
@@ -5,8 +5,10 @@
 #include <dirent.h>
 #include <string.h>
 #include "func.h"
+#include "stack.h"
 
 int ftw(char *pathname, Function *func);
-int dopath(Function *func, char *filename, int len);
+/*int dopath(Function *func, char *filename, int len);*/
+int dopath(Function *func);
 
 #endif /* __FTW_H */
