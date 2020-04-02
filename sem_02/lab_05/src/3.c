b6b1e18f08760ce90ca53665868186213dcfb700
Willsem <happymainer132@yandex.ru>
2020-04-02 15:46:57 +0300

    :pencil: fix 3 programm

--- sem_02/lab_05/src/3.c
+++ sem_02/lab_05/src/3.c
@@ -3,17 +3,17 @@
 int main()
 {
     const char letters[] = "Abcdefghijklmnopqrstuvwxyz";
-    FILE *fd1 = fopen("out.txt", "wr");
-    FILE *fd2 = fopen("out.txt", "wr");
+    FILE *fd[2];
+    fd[0] = fopen("out.txt", "wr");
+    fd[1] = fopen("out.txt", "wr");
 
     for (int i = 0; i < sizeof(letters) - 1; ++i)
     {
-        fprintf(fd1, "%c", letters[i]);
-        fprintf(fd2, "%c" , letters[i]);
+        fprintf(fd[i % 2], "%c", letters[i]);
     }
 
-    fclose(fd1);
-    fclose(fd2);
+    fclose(fd[0]);
+    fclose(fd[1]);
 
     return 0;
 }
